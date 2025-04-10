9c7181cc348cff38fe60e11eb4142baab26457a5
Ivansss7272 <105929547+Ivansss7272@users.noreply.github.com>
2025-04-10 16:16:33 +0200

    cleaning code

--- backend/taskRoutes.py
+++ backend/taskRoutes.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
+
 from task_controller import create_task, get_tasks, update_task, delete_task
 
 load_dotenv()
 
 app = Flask(__name__)
+
 
 @app.route('/tasks', methods=['POST'])
 def create_task_route():
@@ -13,12 +15,14 @@ def create_task_route():
     result = create_task(task_data)
     return jsonify(result), 201
 
+
 @app.route('/tasks', methods=['GET'])
 def get_tasks_route():
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
     tasks = get_tasks(start_date, end_date)
     return jsonify(tasks), 200
+
 
 @app.route('/tasks/<int:task_id>', methods=['PUT'])
 def update_task_route(task_id):
@@ -29,6 +33,7 @@ def update_task_route(task_id):
     else:
         return jsonify({"error": "Task not found."}), 404
 
+
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task_route(task_id):
     result = delete_task(task_id)
@@ -36,6 +41,7 @@ def delete_task_route(task_id):
         return jsonify({"message": "Task deleted successfully."}), 200
     else:
         return jsonify({"error": "Task not found."}), 404
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('HOST', '127.0.0.1'), port=int(os.getenv('PORT', 5000)))
